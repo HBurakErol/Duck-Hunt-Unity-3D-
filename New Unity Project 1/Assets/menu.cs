451fc1ca8f686b896b6a207e8229551bab9a7604
HBurakErol <halitburakerol@gmail.com>
2020-03-26 16:48:14 +0300

    new

---
+++ New Unity Project 1/Assets/menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+   public void oyunubaslat()
+    {
+        Application.LoadLevel(1);
+    }
+    public void oyundancık()
+    {
+        Application.Quit();
+    }
+}
